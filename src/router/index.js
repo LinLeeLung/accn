ab67aaceb031e49529da293713bcfc97897fadee
Daniel <31122395+LinLeeLung@users.noreply.github.com>
2025-06-05 22:42:49 +0800

    'update'

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ const routes = [
     path: "/price",
     name: "Price",
     component: () => import("../components/FuturePrice.vue"),
-    meta: { title: "期貨或工地案估價" },
+    meta: { title: "期貨估價" },
   },
 ];
 
